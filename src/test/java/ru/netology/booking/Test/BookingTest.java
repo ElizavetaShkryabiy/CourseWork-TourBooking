b55f5dec48bae63f1ac8c5d000eaadb452943b1b
Elizaveta <evshkryabiy@gmail.com>
2021-12-20 16:05:06 +0300

    build changed, dependencies added

--- src/test/java/ru/netology/booking/Test/BookingTest.java
+++ src/test/java/ru/netology/booking/Test/BookingTest.java
@@ -1,4 +1,18 @@
 package ru.netology.booking.Test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
 public class BookingTest {
+//    @BeforeAll
+//    static void setUpAll() {
+//        SelenideLogger.addListener("allure", new AllureSelenide());
+//    }
+//
+//    @AfterAll
+//    static void tearDownAll() {
+//        SelenideLogger.removeListener("allure");
+//    }
+
+
 }
